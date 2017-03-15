cc6e5a7a9f8d0dd4dd09002d402790e65b063fcb
Nilook <nkhodabakhsh@horizon.csueastbay.edu>
2017-03-15 14:29:21 -0700

    version2-longscrolling

---
+++ longscrolling/js/script - Copy.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+  //when the document is ready, do a bunch of stuff
+$(window).scrollTop(9800);
+  console.log("your document is ready");
+
+
+var scrollTop = $(window).scrollTop() + $(window).height();
+  $(window).scroll(function(){
+    //when you are scrolling, do a bunch of stuff
+
+    console.log( $(window).scrollTop());
+
+    fromTop = $(window).scrollTop();
+
+    if(fromTop>=9000){
+      $(.cake).css({400 + $(window).scrollTop + "px"});
+    }
+
+    else{
+    $(.cake).css({600 + $(window).scrollTop + "px"});
+    }
+
+    //update the variable fromTop to the current pixels count
+    //fromLeft = $(window).scrollLeft();
+
+  /*  if(fromTop<8000){
+    console.log("gift is flying");
+    //$("#gift").css({"bottom" : fromTop + 300 + "px"});
+    $("#crow").css({"bottom" : fromTop + 30 + "px"});
+  }
+
+  if(fromTop=8500){
+  console.log("gift is here");
+  //$("#gift").css({"bottom" : "0"});
+  $("#crow").css({"animation-play-state": "running"});
+}
+
+if(fromTop<8500){
+    $("#crow").css({"bottom" : "10"});
+}
+
+
+  /*  if (fromTop<6000) {
+      $(".plane").css({"left": "1500px;"})
+    }
+
+
+  if (fromTop<4000) {
+    $(".sun").css({"animation-play-state": "running"})
+
+  }*/
+
+  });//close .scroll
+});//clos .ready
--- longscrolling/js/script.js
+++ longscrolling/js/script.js
@@ -1,43 +1,37 @@
 $(document).ready(function(){
   //when the document is ready, do a bunch of stuff
-$(window).scrollTop(9800);
-  console.log("your document is ready");
+   $(window).scrollTop(9800);
+   console.log("your document is ready");
 
-
-var scrollTop = $(window).scrollTop() + $(window).height();
-  $(window).scroll(function(){
+   var scrollTop = $(window).scrollTop() + $(window).height();
+   $(window).scroll(function(){
     //when you are scrolling, do a bunch of stuff
-
+    console.log( $(window).scrollTop());
     fromTop = $(window).scrollTop();
-    //update the variable fromTop to the current pixels count
-    //fromLeft = $(window).scrollLeft();
 
-  /*  if(fromTop<8000){
-    console.log("gift is flying");
-    //$("#gift").css({"bottom" : fromTop + 300 + "px"});
-    $("#crow").css({"bottom" : fromTop + 30 + "px"});
-  }
+    $(window).scroll(function(){
+  $("#crow").css({"top": 70 + $(window).scrollTop() + "px"});
+  $("#gift").css({"top": 300 + $(window).scrollTop() + "px"});
+});
 
-  if(fromTop=8500){
-  console.log("gift is here");
-  //$("#gift").css({"bottom" : "0"});
-  $("#crow").css({"animation-play-state": "running"});
-}
 
-if(fromTop<8500){
-    $("#crow").css({"bottom" : "10"});
+ if (fromTop > 5000 && fromTop < 6000) {
+  $(".car").css({"animation-play-state" : "running"});
+ }
+
+else{
+  $(".car").css({"animation-play-state" : "paused"});
 }
 
 
-  /*  if (fromTop<6000) {
-      $(".plane").css({"left": "1500px;"})
-    }
+ if (fromTop < 1300) {
+  $(".plane").css({"animation-play-state" : "running"});
+ }
 
+ else{
+   $(".plane").css({"animation-play-state" : "paused"});
+ }
 
-  if (fromTop<4000) {
-    $(".sun").css({"animation-play-state": "running"})
-
-  }*/
 
   });//close .scroll
-});//clos .ready
+});//clos .ready*/
